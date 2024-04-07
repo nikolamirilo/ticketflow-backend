8f4e367c8f644bebba78ea6a3b5bcb86d455251e
Nikola.Mirilo <nikola.mirilo@adacta-fintech.com>
2024-04-07 19:22:06 +0200

    added index routes

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -2,7 +2,28 @@ import userRoutes from "./user.routes.js";
 import eventRoutes from "./event.routes.js";
 import offerRoutes from "./offer.routes.js";
 import orderRoutes from "./order.routes.js";
+import express from "express";
 
-const allRoutes = [eventRoutes, userRoutes, offerRoutes, orderRoutes];
+const indexRoutes = express.Router();
+
+indexRoutes.get("/", function (req, res) {
+  res.send({
+    message:
+      "Ticket Flow backend. For more info about available routes go to /swagger",
+  });
+});
+indexRoutes.get("/test", function (req, res) {
+  res.send({
+    message: "Route used for testing",
+  });
+});
+
+const allRoutes = [
+  indexRoutes,
+  eventRoutes,
+  userRoutes,
+  offerRoutes,
+  orderRoutes,
+];
 
 export default allRoutes;
