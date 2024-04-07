1609da9e4a3e2b7aa251901ff09f626cbd4ccaf8
Nikola.Mirilo <nikola.mirilo@adacta-fintech.com>
2024-04-07 19:29:35 +0200

    removed vercel from project

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import swaggerUi from "swagger-ui-express";
-import swaggerFile from "./swagger_output.json" assert { type: "json" };
+import swaggerFile from "../swagger_output.json" assert { type: "json" };
 import allRoutes from "./routes/index.routes.js";
 import { client } from "./lib/database.config.js";
 import dotenv from "dotenv";
-// import { fetchEvents } from "./web_scrapping/index.scrapping.js";
-// import { deleteEventsTableQuery } from "./queries/event.queries.js";
-// import cron from "node-cron";
-// import { seedEventsTable } from "./seed/index.seed.js";
+import { fetchEvents } from "./web_scrapping/index.scrapping.js";
+import { deleteEventsTableQuery } from "./queries/event.queries.js";
+import cron from "node-cron";
+import { seedEventsTable } from "./seed/index.seed.js";
 
 dotenv.config();
 
@@ -35,17 +35,17 @@ const app = express();
     next();
   });
 
-  // cron.schedule("0 4 * * *", async () => {
-  //   try {
-  //     // Cron job which will get fresh data for events. It will run every day at 4am.
-  //     await client.query(deleteEventsTableQuery);
-  //     const events = await fetchEvents();
-  //     await seedEventsTable(events);
-  //     console.log("Seed Events cron job completed.");
-  //   } catch (error) {
-  //     console.error("Error running fetchEvents cron job:", error);
-  //   }
-  // });
+  cron.schedule("0 4 * * *", async () => {
+    try {
+      // Cron job which will get fresh data for events. It will run every day at 4am.
+      await client.query(deleteEventsTableQuery);
+      const events = await fetchEvents();
+      await seedEventsTable(events);
+      console.log("Seed Events cron job completed.");
+    } catch (error) {
+      console.error("Error running fetchEvents cron job:", error);
+    }
+  });
 
   // For PRODUCTION
   // app.use((req, res, next) => {
