0903427cf9a97711cb166f4438744329ec15ebd3
Nikola Mirilo <nikolamirilo@gmail.com>
2024-07-27 13:27:59 +0200

    fixed config for swagger

--- swagger.js
+++ swagger.js
@@ -3,7 +3,8 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-const protocol = process.env.APP_URL == "localhost:5000" ? "http" : "https";
+const protocol = process.env.APP_URL === "localhost:5000" ? "http" : "https";
+const host = process.env.APP_URL;
 
 const doc = {
   info: {
@@ -11,7 +12,7 @@ const doc = {
     description:
       "Backend for Ticket Flow Application. Below you can find all API endpoints which are served on this server.",
   },
-  host: process.env.APP_URL,
+  host: host,
   schemes: [protocol],
 };
 
